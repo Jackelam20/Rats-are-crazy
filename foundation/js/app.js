56edac3de13e8ad8ead110d8a0852e001d04fa9c
ChrisLlanos <chrisllanos94@gmail.com>
2023-07-31 18:56:17 -0500

    Added wiki api functionality. still need to add text to results card

--- foundation/js/app.js
+++ foundation/js/app.js
@@ -1,5 +1,44 @@
 $(document).foundation(); 
 
+//base url to append query
+const wikiUrl =  "https://en.wikipedia.org/w/api.php?action=query&origin=*&titles=";
+var sampleOption = "Dust_mite_allergy";
+var endpointLast = "&prop=extracts&format=json&exintro=1";
+var wikiFormat = {
+  "Dust Mites": "Dust_mite_allergy",
+  "Mold" : "Mold_health_issues",
+  "Pet Dander" : "Dander",
+  "Cedar" : "Cedrus",
+  "Oak" : "Oak",
+  "Grass" : "Poaceae",
+  "Ragweed" : "Ragweed",
+  "Flowers" : "Flower",
+  "Latex Allergy" : "Latex_allergy",
+  "Cockroach Allergens" : "Cockroach",
+  "Respiratory Irritants" : "Irritation"
+};
+
+function strip(html){
+  let doc = new DOMParser().parseFromString(html, 'text/html');
+  return doc.body.textContent || "";
+}
+
+function displayWiki(option){
+    sampleOption = wikiFormat[option];
+    var completeUrl = wikiUrl+sampleOption+endpointLast;
+    console.log(sampleOption);
+    fetch(completeUrl)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(data){
+        var page = data.query.pages
+        var pageId = Object.keys(data.query.pages)[0];
+        console.log(strip(page[pageId].extract));
+      })
+             
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const enterBtn = document.getElementById("enterBtn");
     const modalContainer = document.getElementById("modalContainer");
@@ -7,6 +46,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const submitModalBtn = document.getElementById("submitModalBtn");
     const resultContainer = document.getElementById("resultContainer");
     const selectedOptionsDiv = document.getElementById("selectedOptions");
+    const wikiPage = document.getElementById("wikiP");
+    
   
     // Event listener to open the modal when the "Enter" button is clicked
     enterBtn.addEventListener("click", function () {
@@ -39,7 +80,10 @@ document.addEventListener("DOMContentLoaded", function () {
         selectedOptionsDiv.innerHTML = "";
         selectedOptions.forEach((option) => {
           selectedOptionsDiv.innerHTML += `<p>${option}</p>`;
+          displayWiki(option);
+
         });
+
       } else {
         alert("Please select at least one option before submitting.");
       }
